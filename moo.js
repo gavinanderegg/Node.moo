a1230b54741f6680e2feddb41d70b95c66158761
Gavin Anderegg <ganderegg@mtlpr.ca>
2012-09-29 13:59:24 -0300

    Hrmm... servers

--- moo.js
+++ moo.js
@@ -2,10 +2,25 @@ var world = {};
 var clients = [];
 
 var underscore = require('underscore');
+
+// var express = require('express');
+// var app = express();
+
 var http = require('http');
 var fs = require('fs');
 
-fs.readFile('./temp.html', function (err, html) {
+// app.get('/', function(req, res) {
+// 	fs.readFile('./index.html', function (err, html) {
+// 		res.set('Content-Type', 'text/html');
+// 		res.send(html);
+// 	});
+// });
+// 
+// app.listen(3000);
+
+
+
+fs.readFile('./index.html', function (err, html) {
 	http.createServer(function(request, response) {  
 		response.writeHeader(200, {"Content-Type": "text/html"});  
 		response.write(html);
